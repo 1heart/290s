e6eeb98f6dc63a6de6c33bd1e484faa0cd62d630
Vincent <vincent.oddrepublic@gmail.com>
2015-01-30 21:20:07 -0800

    app.py changes

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import os
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# forms
+WTF_CSRF_ENABLED = True
+app.secret_key = 'password1337'
+
+
+					# change this to postgresql later
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_repo/info.db'
+db = SQLAlchemy(app)
+
+			# same here
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db_repo/info.db'
+
+from models import *
 
 @app.route('/')
 def index():
